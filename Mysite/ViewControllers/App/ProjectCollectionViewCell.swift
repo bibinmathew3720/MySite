7e0caf04320a165d8f9e7aa6c5f002bf9440b1fa
cs98065 <hari_rk@geojit.com>
2018-03-05 14:24:30 +0530

    Project Collection view base added- hari

---
+++ Mysite/ViewControllers/App/ProjectCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  ProjectCollectionViewCell.swift
+//  Mysite
+//
+//  Created by Hari R Krishna on 05/03/18.
+//  Copyright © 2018 CodeDady. All rights reserved.
+//
+
+import UIKit
+
+class ProjectCollectionViewCell: UICollectionViewCell {
+    
+}
