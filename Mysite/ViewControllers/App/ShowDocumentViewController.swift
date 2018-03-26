6a207b914db322518ac220ebedca69ca688ec921
Bibin Mathew <bibin.m@codelynks.com>
2018-03-26 13:58:14 +0530

    Project image changing option added.

--- Mysite/ViewControllers/App/ShowDocumentViewController.swift
+++ Mysite/ViewControllers/App/ShowDocumentViewController.swift
@@ -8,11 +8,12 @@
 
 import UIKit
 
-class ShowDocumentViewController: UIViewController {
+class ShowDocumentViewController: UIViewController,UINavigationControllerDelegate,UIImagePickerControllerDelegate {
     
     @IBOutlet weak var documentName: UILabel!
     @IBOutlet weak var documentImageView: UIImageView!
     @IBOutlet weak var documentDetails: UILabel!
+    var imagePicker: UIImagePickerController!
     var projectId:String!
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -26,7 +27,20 @@ class ShowDocumentViewController: UIViewController {
         let imageIdentifier = project.projectId! + ".jpg"
         self.documentImageView.image = Utilities().getImage(identifier: imageIdentifier)
     }
-
+    @IBAction func editImageAction(_ sender: UIButton) {
+        imagePicker =  UIImagePickerController()
+        imagePicker.delegate = self
+        imagePicker.sourceType = .camera
+        present(imagePicker, animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        imagePicker.dismiss(animated: true, completion: nil)
+        documentImageView.image = info[UIImagePickerControllerOriginalImage] as? UIImage
+        let imageIdentifier = projectId + ".jpg"
+        Utilities().saveImageDocumentDirectory(image: documentImageView.image!, identifier: imageIdentifier)
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
 
